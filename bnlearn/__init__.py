d80ab9aed71d5bad80abce9f4ac1623cd465ed89
erdogant <erdogant@gmail.com>
2020-01-27 20:37:43 +0100

    parameter learning in sepearte function

--- bnlearn/__init__.py
+++ bnlearn/__init__.py
@@ -1,7 +1,6 @@
 from bnlearn.bnlearn import (
     sampling,
 	inference,
-	parameter_learning,
     import_DAG,
     import_example,
 	to_undirected,
@@ -11,11 +10,16 @@ from bnlearn.bnlearn import (
 
 # Import function in new level
 import bnlearn.structure_learning
+import bnlearn.parameter_learning
 
+import networkx as nx
+import matplotlib as mpl
+assert (nx.__version__)=='1.11', 'This function requires networkx to be v1.11. Try to: pip install networkx==v1.11'
+assert (mpl.__version__)=='2.2.3', 'This function requires matplotlib to be v2.2.3. Try to: pip install matplotlib==v2.2.3'
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.1.4'
+__version__ = '0.2.4'
 
 # module level doc-string
 __doc__ = """
--- bnlearn/bnlearn.py
+++ bnlearn/bnlearn.py
@@ -172,124 +172,18 @@ def inference(model, variables=None, evidence=None, verbose=3):
     the summation only factors that involved that variable and hence avoiding computing the
     complete joint distribution.
     """
-    
+    if verbose>=3: print('[BNLEARN][inference] Variable Elimination..')
     model_infer = VariableElimination(model['model'])
     # Computing the probability of Wet Grass given Rain.
     q = model_infer.query(variables=variables, evidence=evidence)
     print(q)
-    # for varname in variables: 
-        # print(q[varname])
+    # for varname in variables: print(q[varname])
     return(q)
 
 
 # %% Sampling from model
-def parameter_learning(model, df, methodtype='bayes', verbose=3):
-    """Parameter Learning.
-    
-    Description
-    ----------
-    Parameter learning is the task to estimate the values of the conditional probability distributions (CPDs), 
-    for the variables cloudy, sprinkler, rain and wet grass.
-    State counts
-        To make sense of the given data, we can start by counting how often each state of the variable occurs.
-        If the variable is dependent on parents, the counts are done conditionally on the parents states,
-        i.e. for seperately for each parent configuration
-
-
-    Parameters
-    ----------
-    model       : [DICT] Contains model and adjmat.
-
-    df          : [pd.DataFrame] Pandas DataFrame containing the data
-                   f1  ,f2  ,f3
-                s1 0   ,0   ,1
-                s2 0   ,1   ,0
-                s3 1   ,1   ,0
-
-    methodtype  : [STRING] strategy for parameter learning.
-                'nl' or 'maximumlikelihood' (default) :Learning CPDs using Maximum Likelihood Estimators
-                'bayes' :Bayesian Parameter Estimation
-
-    verbose : int [0-5] (default: 3)
-        Print messages to screen.
-        0: NONE
-        1: ERROR
-        2: WARNING
-        3: INFO (default)
-        4: DEBUG
-        5: TRACE
-
-    Returns
-    -------
-    model
-
-    """
-
-    config = dict()
-    config['verbose'] = verbose
-    config['method'] = methodtype
-    model = model['model']
-    if verbose>=3: print('[BNLEARN][PARAMETER LEARNING] Computing parameters using [%s]' %(config['method']))
-
-#    pe = ParameterEstimator(model, df)
-#    print("\n", pe.state_counts('Cloudy'))
-#    print("\n", pe.state_counts('Sprinkler'))
-
-    """
-    Maximum Likelihood Estimation
-        A natural estimate for the CPDs is to simply use the *relative frequencies*,
-        with which the variable states have occured. We observed x cloudy` among a total of `all clouds`,
-        so we might guess that about `50%` of `cloudy` are `sprinkler or so.
-        According to MLE, we should fill the CPDs in such a way, that $P(\text{data}|\text{model})$ is maximal.
-        This is achieved when using the *relative frequencies*.
-
-    While very straightforward, the ML estimator has the problem of *overfitting* to the data.
-    If the observed data is not representative for the underlying distribution, ML estimations will be extremly far off.
-    When estimating parameters for Bayesian networks, lack of data is a frequent problem.
-    Even if the total sample size is very large, the fact that state counts are done conditionally
-    for each parents configuration causes immense fragmentation.
-    If a variable has 3 parents that can each take 10 states, then state counts will
-    be done seperately for `10^3 = 1000` parents configurations.
-    This makes MLE very fragile and unstable for learning Bayesian Network parameters.
-    A way to mitigate MLE's overfitting is *Bayesian Parameter Estimation*.
-    """
-
-    # Learning CPDs using Maximum Likelihood Estimators
-    if config['method']=='ml' or config['method']=='maximumlikelihood':
-        mle = MaximumLikelihoodEstimator(model, df)
-        for node in mle.state_names:
-            print(mle.estimate_cpd(node))
-
-
-    """
-    Bayesian Parameter Estimation
-        The Bayesian Parameter Estimator starts with already existing prior CPDs,
-        that express our beliefs about the variables *before* the data was observed.
-        Those "priors" are then updated, using the state counts from the observed data.
-    
-    One can think of the priors as consisting in *pseudo state counts*, that are added
-    to the actual counts before normalization. Unless one wants to encode specific beliefs
-    about the distributions of the variables, one commonly chooses uniform priors,
-    i.e. ones that deem all states equiprobable.
-    
-    A very simple prior is the so-called *K2* prior, which simply adds `1` to the count of every single state.
-    A somewhat more sensible choice of prior is *BDeu* (Bayesian Dirichlet equivalent uniform prior).
-    For BDeu we need to specify an *equivalent sample size* `N` and then the pseudo-counts are
-    the equivalent of having observed `N` uniform samples of each variable (and each parent configuration).
-    """
-    if config['method']=='bayes':
-        model.fit(df, estimator=BayesianEstimator, prior_type="BDeu", equivalent_sample_size=1000) # default equivalent_sample_size=5
-
-        for cpd in model.get_cpds():
-            if verbose>=3: print("CPD of {variable}:".format(variable=cpd.variable))
-            if verbose>=3: print(cpd)
-
-    return(model)
-
-
-# %% Sampling from model
 def sampling(model, n=1000, verbose=3):
-    """Sampling based on DAG.
+    """Sample based on DAG.
 
     Parameters
     ----------
