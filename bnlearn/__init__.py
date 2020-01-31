2a2b8f81e251ae3132f88813bec52ef5a8356fe9
erdogant <erdogant@gmail.com>
2020-01-31 22:07:59 +0100

    accidental version correction

--- bnlearn/__init__.py
+++ bnlearn/__init__.py
@@ -14,12 +14,12 @@ import bnlearn.inference
 
 import networkx as nx
 import matplotlib as mpl
-assert (nx.__version__)=='1.11', 'This function requires networkx to be v1.11. Try to: pip install networkx==v1.11'
-assert (mpl.__version__)=='2.2.3', 'This function requires matplotlib to be v2.2.3. Try to: pip install matplotlib==v2.2.3'
+#assert (nx.__version__)=='1.11', 'This function requires networkx to be v1.11. Try to: pip install networkx==v1.11'
+#assert (mpl.__version__)=='2.2.3', 'This function requires matplotlib to be v2.2.3. Try to: pip install matplotlib==v2.2.3'
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.2.4'
+__version__ = '0.2.1'
 
 # module level doc-string
 __doc__ = """
