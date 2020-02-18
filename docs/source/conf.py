0e330529b8ae85223f36b0c0b7c42f01951896a9
erdogant <erdogant@gmail.com>
2020-02-18 09:16:30 +0100

    experimental changed in conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,11 +31,19 @@ release = 'bnlearn'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# extensions = [
+#     "sphinx.ext.autodoc",
+#     "sphinx.ext.napoleon",
+#     "sphinx.ext.viewcode",
+#     "sphinx.ext.todo",
+# ]
+
 extensions = [
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
     "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
     "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.todo",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
