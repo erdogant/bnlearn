551e7d14a2b6bd33bb5ddd3b89ddf24f04bccab0
Erdogan Taskesen <erdogant@gmail.com>
2020-09-19 23:05:12 +0200

    update cleaning script

--- make_clean.sh
+++ make_clean.sh
@@ -2,4 +2,19 @@ echo "Cleaning previous builds first.."
 rm -rf dist
 rm -rf build
 rm -rf bnlearn.egg-info
+rm -rf __pycache__
+rm -rf .pytest_cache
+rm -rf .pylint.d
 rm -rf bnlearn/data/*.zip
+rm -rf bnlearn/data/*.csv
+rm -rf bnlearn/__pycache__
+rm -rf bnlearn/helpers/__pycache__
+rm -rf bnlearn/tests/__pycache__
+rm -rf *.js
+rm -rf *.html
+rm -rf *.css
+rm -rf *.dot
+rm -rf *.png
+
+
+
