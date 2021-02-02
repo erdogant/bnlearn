60f8a15be33cd93febc08c4f0c00e305f4b700c1
erdogant <erdogant@gmail.com>
2021-02-02 16:32:20 +0100

    0.3.13

--- bnlearn/__init__.py
+++ bnlearn/__init__.py
@@ -40,7 +40,7 @@ if not version.parse(nx.__version__) >= version.parse("2.5"):
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.3.12'
+__version__ = '0.3.13'
 
 # module level doc-string
 __doc__ = """
