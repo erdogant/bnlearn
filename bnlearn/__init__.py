898888c28a5e734cb5e4d5e2a3dcf7b6600958f1
erdogant <erdogant@gmail.com>
2022-01-04 20:01:44 +0100

    update docstring

--- bnlearn/__init__.py
+++ bnlearn/__init__.py
@@ -62,17 +62,13 @@ BNLEARN - bnlearn is an Python package for learning the graphical structure of B
 Description
 -----------
 * Learning a Bayesian network can be split into two problems:
-    * Parameter learning: Given a set of data samples and a DAG that captures the dependencies between the variables,
-      estimate the (conditional) probability distributions of the individual variables.
     * Structure learning: Given a set of data samples, estimate a DAG that captures the dependencies between the variables.
-* Currently, the library supports:
-    * Parameter learning for *discrete* nodes:
-    * Maximum Likelihood Estimation
-    * Bayesian Estimation
-* Structure learning for *discrete*, *fully observed* networks:
-    * Score-based structure estimation (BIC/BDeu/K2 score; exhaustive search, hill climb/tabu search)
-    * Constraint-based structure estimation (PC)
-    * Hybrid structure estimation (MMHC)
+    * Parameter learning: Given a set of data samples and a DAG that captures the dependencies between the variables.
+    * Making inferences.
+    * Parameter and structure learning is for *discrete* nodes
+        * Score-based structure estimation (BIC/BDeu/K2 score; exhaustive search, hill climb/tabu search)
+        * Constraint-based structure estimation (PC)
+        * Hybrid structure estimation (MMHC)
 
 
 Example
@@ -113,5 +109,7 @@ References
 ----------
 * https://bnlearn.readthedocs.io
 * https://github.com/erdogant/bnlearn
+* https://towardsdatascience.com/a-step-by-step-guide-in-detecting-causal-relationships-using-bayesian-structure-learning-in-python-c20c6b31cee5
+* https://towardsdatascience.com/a-step-by-step-guide-in-designing-knowledge-driven-models-using-bayesian-theorem-7433f6fd64be
 
 """
