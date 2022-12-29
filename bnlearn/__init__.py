f888c2ae4a31bf9334467df386684e367f5e09a6
erdogant <erdogant@gmail.com>
2022-12-29 20:53:24 +0100

    set cdn_resources "inline" when using jupyter notebook. Default: "local"

--- bnlearn/__init__.py
+++ bnlearn/__init__.py
@@ -37,7 +37,7 @@ from packaging import version
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.7.11'
+__version__ = '0.7.12'
 
 try:
     import pgmpy
--- bnlearn/bnlearn.py
+++ bnlearn/bnlearn.py
@@ -1019,7 +1019,7 @@ def plot(model,
         return None
 
     # Plot properties
-    defaults = {'height': '800px', 'width': '70%', 'notebook': False, 'layout': None, 'font_color': False, 'bgcolor': '#ffffff', 'directed': True}
+    defaults = {'height': '800px', 'width': '70%', 'notebook': False, 'layout': None, 'font_color': False, 'bgcolor': '#ffffff', 'directed': True, 'cdn_resources': 'local', 'select_menu': False, 'filter_menu': False}
     params_interactive = {**defaults, **params_interactive}
     defaults = {'minscale': 1, 'maxscale': 10, 'figsize': (15, 10), 'height': None, 'width': None, 'font_size': 14, 'font_family': 'sans-serif', 'alpha': 0.8, 'layout': 'spring_layout', 'font_color': 'k', 'facecolor': '#ffffff', 'node_shape': 'o', 'edge_alpha': 0.8, 'arrowstyle': '-|>', 'arrowsize': 30, 'visible': True}
     params_static = {**defaults, **params_static}
@@ -1112,6 +1112,9 @@ def _plot_static(model, params_static, nodelist, node_colors, node_sizes, G, pos
 
 # %% Plot interactive
 def _plot_interactive(model, params_interactive, nodelist, node_colors, node_sizes, edgelist, edge_colors, edge_weights, title, verbose=3):
+    if not params_interactive['notebook']:
+        # https://pyvis.readthedocs.io/en/latest/tutorial.html?highlight=cdn_resources#using-pyvis-within-jupyter-notebook
+        params_interactive['cdn_resources']='inline'
     try:
         from pyvis.network import Network
         from IPython.core.display import display, HTML
