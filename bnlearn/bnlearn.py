a504e53eb25c53156b1efb7718cc3f1876702e9e
erdogant <erdogant@gmail.com>
2023-05-17 12:59:44 +0200

    comment the logging for future purposes

--- bnlearn/bnlearn.py
+++ bnlearn/bnlearn.py
@@ -32,22 +32,20 @@ import datazets as dz
 import pypickle
 import bnlearn
 
-logger = logging.getLogger('')
-for handler in logger.handlers[:]: #get rid of existing old handlers
-    logger.removeHandler(handler)
-console = logging.StreamHandler()
-formatter = logging.Formatter('[bnlearn] >%(levelname)s> %(message)s')
-console.setFormatter(formatter)
-logger.addHandler(console)
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger('')
+# for handler in logger.handlers[:]: #get rid of existing old handlers
+#     logger.removeHandler(handler)
+# console = logging.StreamHandler()
+# formatter = logging.Formatter('[bnlearn] >%(levelname)s> %(message)s')
+# console.setFormatter(formatter)
+# logger.addHandler(console)
+# logger = logging.getLogger(__name__)
 
 
 # %%  Convert adjmat to bayesian model
 def to_bayesiannetwork(model, verbose=3):
     """Convert adjacency matrix to BayesianNetwork.
 
-    Description
-    -----------
     Convert a adjacency to a Bayesian model. This is required as some of the
     functionalities, such as ``structure_learning`` output a DAGmodel.
     If the output of ``structure_learning`` is provided, the adjmat is extracted and processed.
@@ -1793,50 +1791,50 @@ def structure_scores(model, df, scoring_method=['k2', 'bds', 'bic', 'bdeu'], ver
 
 
 # %%
-def set_logger(verbose: [str, int] = 'info'):
-    """Set the logger for verbosity messages.
+# def set_logger(verbose: [str, int] = 'info'):
+#     """Set the logger for verbosity messages.
 
-    Parameters
-    ----------
-    verbose : [str, int], default is 'info' or 20
-        Set the verbose messages using string or integer values.
-        * [0, 60, None, 'silent', 'off', 'no']: No message.
-        * [10, 'debug']: Messages from debug level and higher.
-        * [20, 'info']: Messages from info level and higher.
-        * [30, 'warning']: Messages from warning level and higher.
-        * [50, 'critical']: Messages from critical level and higher.
+#     Parameters
+#     ----------
+#     verbose : [str, int], default is 'info' or 20
+#         Set the verbose messages using string or integer values.
+#         * [0, 60, None, 'silent', 'off', 'no']: No message.
+#         * [10, 'debug']: Messages from debug level and higher.
+#         * [20, 'info']: Messages from info level and higher.
+#         * [30, 'warning']: Messages from warning level and higher.
+#         * [50, 'critical']: Messages from critical level and higher.
 
-    Returns
-    -------
-    None.
+#     Returns
+#     -------
+#     None.
 
-    > # Set the logger to warning
-    > set_logger(verbose='warning')
-    > # Test with different messages
-    > logger.debug("Hello debug")
-    > logger.info("Hello info")
-    > logger.warning("Hello warning")
-    > logger.critical("Hello critical")
+#     > # Set the logger to warning
+#     > set_logger(verbose='warning')
+#     > # Test with different messages
+#     > logger.debug("Hello debug")
+#     > logger.info("Hello info")
+#     > logger.warning("Hello warning")
+#     > logger.critical("Hello critical")
 
-    """
-    # Set 0 and None as no messages.
-    if (verbose==0) or (verbose is None):
-        verbose=60
-    # Convert str to levels
-    if isinstance(verbose, str):
-        levels = {'silent': 60,
-                  'off': 60,
-                  'no': 60,
-                  'debug': 10,
-                  'info': 20,
-                  'warning': 30,
-                  'critical': 50}
-        verbose = levels[verbose]
+#     """
+#     # Set 0 and None as no messages.
+#     if (verbose==0) or (verbose is None):
+#         verbose=60
+#     # Convert str to levels
+#     if isinstance(verbose, str):
+#         levels = {'silent': 60,
+#                   'off': 60,
+#                   'no': 60,
+#                   'debug': 10,
+#                   'info': 20,
+#                   'warning': 30,
+#                   'critical': 50}
+#         verbose = levels[verbose]
 
-    # Show examples
-    logger.setLevel(verbose)
+#     # Show examples
+#     logger.setLevel(verbose)
 
-# %%
-def disable_tqdm():
-    """Set the logger for verbosity messages."""
-    return (True if (logger.getEffectiveLevel()>=30) else False)
+# # %%
+# def disable_tqdm():
+#     """Set the logger for verbosity messages."""
+#     return (True if (logger.getEffectiveLevel()>=30) else False)
