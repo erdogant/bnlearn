5a01da3b7ac619e823bd9ff8ab1b71e6cc471eff
erdogant <erdogant@gmail.com>
2023-11-10 22:24:22 +0100

    update vec2adjmat function for speed improvements

--- bnlearn/bnlearn.py
+++ bnlearn/bnlearn.py
@@ -382,7 +382,7 @@ def vec2df(source, target, weights=None):
 
 
 # %%  Convert adjacency matrix to vector
-def vec2adjmat(source, target, weights=None, symmetric=True):
+def vec2adjmat(source, target, weight=None, symmetric: bool = True, aggfunc='sum', verbose=3) -> pd.DataFrame:
     """Convert source and target into adjacency matrix.
 
     Parameters
@@ -391,10 +391,13 @@ def vec2adjmat(source, target, weights=None, symmetric=True):
         The source node.
     target : list
         The target node.
-    weights : list of int
+    weight : list of int
         The Weights between the source-target values
     symmetric : bool, optional
         Make the adjacency matrix symmetric with the same number of rows as columns. The default is True.
+    aggfunc : str, optional
+        Aggregate function in case multiple values exists for the same relationship.
+        'sum' (default)
 
     Returns
     -------
@@ -403,44 +406,65 @@ def vec2adjmat(source, target, weights=None, symmetric=True):
 
     Examples
     --------
-    >>> import bnlearn as bn
-    >>> source=['Cloudy','Cloudy','Sprinkler','Rain']
-    >>> target=['Sprinkler','Rain','Wet_Grass','Wet_Grass']
+    >>> source = ['Cloudy', 'Cloudy', 'Sprinkler', 'Rain']
+    >>> target = ['Sprinkler', 'Rain', 'Wet_Grass', 'Wet_Grass']
     >>> vec2adjmat(source, target)
-    >>> weights=[1,2,1,3]
-    >>> adjmat = bn.vec2adjmat(source, target, weights=weights)
+
+    >>> weight = [1, 2, 1, 3]
+    >>> vec2adjmat(source, target, weight=weight)
 
     """
-    if len(source)!=len(target): raise ValueError('[hnet] >Source and Target should have equal elements.')
-    if weights is None: weights = [1] *len(source)
+    if len(source) != len(target): raise ValueError('[d3graph] >Source and Target should have equal elements.')
+    if weight is None: weight = [1] * len(source)
+    if verbose>=3: print('[bnlearn] >Converting source-target into adjacency matrix..')
 
     df = pd.DataFrame(np.c_[source, target], columns=['source', 'target'])
     # Make adjacency matrix
-    adjmat = pd.crosstab(df['source'], df['target'], values=weights, aggfunc='sum').fillna(0)
+    adjmat = pd.crosstab(df['source'], df['target'], values=weight, aggfunc=aggfunc).fillna(0)
     # Get all unique nodes
-    nodes = np.unique(list(adjmat.columns.values) +list(adjmat.index.values))
+    nodes = np.unique(list(adjmat.columns.values) + list(adjmat.index.values))
     # nodes = np.unique(np.c_[adjmat.columns.values, adjmat.index.values].flatten())
 
     # Make the adjacency matrix symmetric
     if symmetric:
-        # Add missing columns
-        node_columns = np.setdiff1d(nodes, adjmat.columns.values)
-        for node in node_columns:
-            adjmat[node]=0
+        if verbose>=3: print('[bnlearn] >Making the matrix symmetric..')
+        # # Add missing columns
+        # node_columns = np.setdiff1d(nodes, adjmat.columns.values)
+        # for node in node_columns:
+        #     adjmat[node] = 0
 
-        # Add missing rows
-        node_rows = np.setdiff1d(nodes, adjmat.index.values)
-        adjmat=adjmat.T
-        for node in node_rows:
-            adjmat[node]=0
-        adjmat=adjmat.T
+        # # Add missing rows
+        # node_rows = np.setdiff1d(nodes, adjmat.index.values)
+        # adjmat = adjmat.T
+        # for node in node_rows:
+        #     adjmat[node] = 0
+
+        # Add missing columns
+        IA, _ = ismember(nodes, adjmat.columns.values)
+        node_columns = nodes[~IA]
+        if len(node_columns) > 0:
+            df_new_columns = pd.DataFrame(0, index=adjmat.index, columns=node_columns)
+            adjmat = pd.concat([adjmat, df_new_columns], axis=1)
+
+        # # Add missing rows
+        IA, _ = ismember(nodes, adjmat.index.values)
+        node_rows = nodes[~IA]
+        # node_rows = np.setdiff1d(nodes, adjmat.index.values)
+        if len(node_rows) > 0:
+            df_new_rows = pd.DataFrame(0, index=node_rows, columns=adjmat.columns)
+            adjmat = pd.concat([adjmat, df_new_rows], axis=0)
+
+        # adjmat = adjmat.T
 
         # Sort to make ordering of columns and rows similar
-        [IA, IB] = ismember(adjmat.columns.values, adjmat.index.values)
+        if verbose>=4: print('[bnlearn] >Order columns and rows.')
+        _, IB = ismember(adjmat.columns.values, adjmat.index.values)
         adjmat = adjmat.iloc[IB, :]
-        adjmat.index.name='source'
-        adjmat.columns.name='target'
+        adjmat.index.name = 'source'
+        adjmat.columns.name = 'target'
 
+    # Force columns to be string type
+    adjmat.columns = adjmat.columns.astype(str)
     return adjmat
 
 
