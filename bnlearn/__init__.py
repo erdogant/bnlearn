30e90d3054c61a2a7e4a3bfa68f1abea12f2c524
erdogant <erdogant@gmail.com>
2024-10-23 23:41:32 +0200

    Fix for another bug.

--- bnlearn/__init__.py
+++ bnlearn/__init__.py
@@ -40,7 +40,7 @@ from packaging import version
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.10.1'
+__version__ = '0.10.2'
 
 import pgmpy
 # Check version pgmpy
