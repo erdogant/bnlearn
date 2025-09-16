b2f2ecbff144a447438632d772f940cb0cc2ba82
erdogant <erdogant@gmail.com>
2025-09-16 19:56:26 +0200

    cleaning

--- julia/Learn_Discrete_BayesNet.jl
+++
@@ -1,7 +0,0 @@
-module Learn_Discrete_BayesNet
-
-# package code goes here
-
-include("L_DVBN.jl")
-
-end # module
--- julia/runtests.jl
+++
@@ -1,5 +0,0 @@
-using Learn_Discrete_BayesNet
-using Base.Test
-
-# Test1: Auto MPG dataset
-include("test_on_AutoMPG_dataset.jl")
