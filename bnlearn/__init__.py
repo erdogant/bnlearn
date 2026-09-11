0b7525e1d6e1332fc05af01cb323be1950bbece5
erdogant <erdogant@gmail.com>
2026-09-11 22:23:20 +0200

    update docs

--- bnlearn/__init__.py
+++ bnlearn/__init__.py
@@ -86,7 +86,7 @@ from packaging import version
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.14.2'
+__version__ = '0.15.0'
 
 import pgmpy
 # Check version pgmpy
